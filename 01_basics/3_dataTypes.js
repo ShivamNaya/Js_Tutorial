192e986251715ab87233d604ba5dab0858041e4e
Shivam Nayak <160408038+ShivamNaya@users.noreply.github.com>
2024-02-24 18:18:24 +0000

    dataTypes

---
+++ 01_basics/3_dataTypes.js
@@ -0,0 +1,7 @@
+let name = "Shivam Nayak"
+let age = 20
+let isloggedIn = false
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null ); // object
+console.log(typeof age ); // number
